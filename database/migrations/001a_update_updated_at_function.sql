109451d5444f9fcbef87b873a633d20119c33e15
Abdullah Al Baky <mrk3218@gmail.com>
2026-07-12 15:16:04 +0600

    DB update

---
+++ database/migrations/001a_update_updated_at_function.sql
@@ -0,0 +1,13 @@
+-- =====================================================
+-- Shared updated_at trigger function
+-- =====================================================
+
+create or replace function public.update_updated_at_column()
+returns trigger
+language plpgsql
+as $$
+begin
+    new.updated_at = now();
+    return new;
+end;
+$$;
