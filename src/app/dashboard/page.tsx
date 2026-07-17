3355a874b0be4ac36f9e1ade77e50dd413d79fcc
Baky1055 <abdullah.ctrendssqa08@gmail.com>
2026-07-17 20:13:18 +0600

    feat(auth): complete authentication foundation

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function DashboardPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">
+        Student Dashboard
+      </h1>
+
+      <p className="mt-4 text-muted-foreground">
+        Welcome to SQA with Abdullah.
+      </p>
+    </div>
+  );
+}
